7f8d622d004bb8eb89130055faac22e4828c2fbb
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-05-15 13:26:15 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from "./components/Header";
+import CalculatePercentage from "./components/CalculatePercentage";
+import GetPercentage from "./components/GetPercentage";
+import { Typography } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+
+  constructor() {
+    super()
+
+    this.state = {
+      answer: "",
+      answer2: "",
+    }
+  }
+
+  calculatePercentage = (percentage, amount) => {
+
+    const answer = percentage / 100 * amount;
+
+    this.setState({
+      answer: answer,
+    })
+  }
+
+  getPercentage = (amount, amount2) => {
+    const answer2 = 100 / amount2 * amount;
+
+    this.setState({
+      answer2: answer2 +"%",
+    })
+  }
+
+
+  render() {
+    const { answer, answer2 } = this.state;
+    return (
+      <div className="App">
+        <Header />
+        <CalculatePercentage calculatePercentage={this.calculatePercentage} />
+        <div className="container">
+          <Typography variant="h3">Answer is: {answer}</Typography>
+        </div>
+        <GetPercentage getPercentage={this.getPercentage} />
+        <div className="container">
+          <Typography variant="h3">Answer is: {answer2}</Typography>
+        </div>
+      </div>
+    );
+
+  }
 }
 
 export default App;
+
+
+
+/*
+
+CALCULATION:
+
+percentage /  100 * a = amount
+
+100 / amount * a = needed
+
+*/
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <nav className="nav-bar">
+                <h1>Percentage Calculator</h1>
+            </nav>
+        )
+    }
+}
